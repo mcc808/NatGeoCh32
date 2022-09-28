1b91728c7502c6155d6fddcca949c94bacfbb90b
mcc808 <112995753+mcc808@users.noreply.github.com>
2022-09-27 19:19:46 -0700

    working with js

---
+++ jsScript.js
@@ -0,0 +1,49 @@
+console.log("here i am again");
+
+//var myfirstVar = "Manuel";
+//var age=36; 
+//myfirstVar = "Manuel";
+
+const musicGenre = "Rock";
+
+let artistName0 = "Phil Collins";
+let instrument0 = "Piano";
+let selection0 = 0;
+let songName0 = "In the Air Tonight";
+let isAvailable0 = true;  
+let yearReleased0 = 1981;
+let freePlay0 = true;
+let download0 = false;
+let downloads0 = 100;
+let popularity0 = "high";
+
+
+console.log(selection0);
+
+document.write('You have selected: '+ artistName0 + ', on track: ' + selection0);
+
+let artistName1 = "Sting"
+let instrument1 = "Drums"
+let selection1 = 1;
+let songName1 = "Desert Rose";
+let isAvailable1 = true; 
+let yearReleased1 = 1999;
+let freePlay1 = true; 
+let download1 = false;
+let downloads1 = 99;
+let popularity1 = "high"; 
+
+
+
+console.log(selection1);
+
+document.write('<p>You have selected: '+ artistName1 + ',  on track: ' + selection1);
+
+
+//console.log("My name is:"+ userName + "and my age is"+ age);
+//console.log("the constant value is:" + tax);
+
+//document.write('<p>Name :+"userName"  and the age is:"+age+"</p>');
+
+//document.write('<p>Name: "${userName}"</p> <p>age is: "${age}"</p>');
+
